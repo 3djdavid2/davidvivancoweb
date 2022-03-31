45fda93c476e71c5cbddb35c13a7e0c926ce877a
3djDavid <dj.vivanco@gmail.com>
2022-03-31 09:09:01 -0300

    baseurl

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  baseUrl: '34.198.236.213/:3000/send-email'
+  baseUrl: 'http://34.198.236.213/:3000/send-email'
   // baseUrl: 'http://localhost:3000/send-email'
 };
 
