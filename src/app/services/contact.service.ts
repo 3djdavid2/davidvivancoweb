56eb2e337783d046d1e94b0e29f648aa5d3ec22c
3djDavid <dj.vivanco@gmail.com>
2022-03-30 23:00:38 -0300

    baseurl

--- src/app/services/contact.service.ts
+++ src/app/services/contact.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContactService {
 
-  URI = 'http://localhost:3000/send-email'
+  URI = environment.baseUrl
 
   constructor(private http: HttpClient) { }
 
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  baseUrl: '34.198.236.213/'
 };
